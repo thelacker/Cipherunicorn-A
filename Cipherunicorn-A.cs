515332b78154823f99942445e5abc615f0c98db9
thelacker <dyatlikos3000@ya.ru>
2014-10-04 20:24:56 +0400

    Добавлен файл Cipherunicorn-A
    Файл с функцией main

---
+++ Cipherunicorn-A.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.SymmetricAlgorithm;
+
+namespace Cipherunicon_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+namespace Console_Out
+{
+    class Errors
+    {
+        void WrongArguments()
+        {
+            Console.WriteLine("Wrong Arguments!");
+        }
+        void TrownException(Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
