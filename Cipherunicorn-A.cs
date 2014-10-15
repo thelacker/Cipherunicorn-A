0195c9ad1d2bfff94ab2a6b07a3ad3871d35e1b9
thelacker <dyatlikos3000@ya.ru>
2014-10-16 00:53:29 +0400

    Добавление класса информации для шифрования
    Пути к файлам, тип операции

--- Cipherunicorn-A.cs
+++ Cipherunicorn-A.cs
@@ -3,74 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Security.Cryptography.SymmetricAlgorithm;
+using System.IO;
 
 namespace Cipherunicon_A
 {
-	// класс программы
-    class Program 
+    // класс программы
+    class Program
     {
-    	// функция main
-        static void Main(string[] args) 
+        // функция main
+        static void Main(string[] args)
         {
-            
+
         }
     }
 
-    //TODO: дописать комментарии
-    // класс аргументов командной строки
-    class Args
+    // класс 
+    class DataForCrypting
     {
-        private string _opKey;          // ключ операции
-        private string _fileKey;        // ключ файла
-        private string _inPath;         // путь к входному файлу
-        private string _outPath;        // путь к выходному файлу
-
-        public string opKey
-        {
-            get
-            {
-                return _opKey;
-            }
-        }          //
-        public string fileKey
-        {
-            get
-            {
-                return _fileKey;
-            }
-        }        //
-        public string inPath
-        {
-            get
-            {
-                return _inPath;
-            }
-        }         //
-        public string outPath
-        {
-            get
-            {
-                return _outPath;
-            }
-        }        //
+        public string input;        // путь к файлу ввода
+        public string output;       // путь к файлу вывода
+        public bool   optype;       // тип операции
 
         // конструктор класса
-        private Args(string op, string file, string inf, string outf){
-            _opKey      =   op;
-            _fileKey    =   file;
-            _inPath     =   inf;
-            _outPath    =   outf;
-        }
-
-        private Args instance;
-        public Args Instance(string op, string file, string inf, string outf)
+        public DataForCrypting() 
         {
-            if (instance == null)
-            {
-                instance = new Args(op,file,inf,outf);
-            }
-            return instance;
+            input   =   null;
+            output  =   null;
+            optype  =   false;
         }
-     }
+    }
 }
